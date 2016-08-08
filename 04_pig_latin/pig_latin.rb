7f4850e566d044bf5788f28340492302b26c7a90
Courtney Wilkins <seattlecrw@yahoo.com>
2016-08-07 20:50:15 -0700

    submit answer 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,18 @@
 #write your code here
+def translate string
+	arr = string.split(' ')
+
+	new_arr = []
+	arr.each do |word|
+		first_vowel = word.downcase.index(/[aeiou]/)
+		if first_vowel == 0
+			new_arr.push(word + 'ay')
+		elsif word[first_vowel-1..first_vowel] == 'qu'
+			second_vowel = word.downcase.index(/[aeiou]/, first_vowel + 1)
+			new_arr.push(word[second_vowel..word.length] + word[0...second_vowel] + 'ay')
+		else
+			new_arr.push(word[first_vowel..word.length] + word[0...first_vowel] + 'ay')
+		end
+	end
+	return new_arr.join(' ')
+end
