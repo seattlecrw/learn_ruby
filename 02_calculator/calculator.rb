922eef2e61757ba91c097d28dda1e823f56dcb10
Courtney Wilkins <seattlecrw@yahoo.com>
2016-08-07 19:38:36 -0700

    submit answer to 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add num1, num2
+	return num1 + num2
+end
+
+def subtract num1, num2
+	return num1 - num2
+end
+
+def sum arr
+	total = 0
+	arr.each do |x|
+		total += x
+	end
+	return total
+end
