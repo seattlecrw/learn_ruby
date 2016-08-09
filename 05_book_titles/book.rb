74d9fffcc078703270de9d90adad35d988f9dc27
Courtney Wilkins <seattlecrw@yahoo.com>
2016-08-08 20:47:43 -0700

    submit answer for 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
 # write your code here
+	attr_reader :title
+
+	def title= name
+		arr = name.split(' ')
+		conjunctions = ['a', 'an', 'and', 'or', 'in', 'of', 'the']
+
+		arr.each do |word|
+			if !conjunctions.include?(word) or arr[0] == word
+				word.capitalize!
+			end
+		end
+		@title = arr.join(' ')
+	end
 end
