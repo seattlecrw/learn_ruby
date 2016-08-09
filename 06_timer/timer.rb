618ce42f357d67db04ea635831f9db6a82f7497a
Courtney Wilkins <seattlecrw@yahoo.com>
2016-08-08 21:15:17 -0700

    submit solution to 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+  	@seconds = 0
+  end
+
+  def time_string
+  	hrs = @seconds / 3600
+  	left = @seconds % 3600
+  	mins = left / 60
+  	secs = left % 60
+  	return "%02d:%02d:%02d" % [hrs, mins, secs]
+  end
 end
